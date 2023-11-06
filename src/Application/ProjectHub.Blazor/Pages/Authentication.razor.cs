0876ec3b3dbfba918e2ca9d511922fb24e18c923
Pierre Meyer <p.meyer@neusta.de>
2023-11-06 07:09:13 +0000

    Feature/azure ad login

---
+++ src/Application/ProjectHub.Blazor/Pages/Authentication.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using ProjectHub.Blazor.Constants;
+
+namespace ProjectHub.Blazor.Pages;
+
+public partial class Authentication
+{
+    [Parameter] public string? Action { get; set; }
+
+    private void LoginSuccessHandler(RemoteAuthenticationState state)
+    {
+        state.ReturnUrl = ProjectHubRoute.LandingPage;
+    }
+
+    private void LogoutSuccessHandler(RemoteAuthenticationState state)
+    {
+        state.ReturnUrl = ProjectHubRoute.LogoutPage;
+    }
+}
---
+++ src/Application/ProjectHub.Blazor/Shared/LoginDisplay.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using ProjectHub.Blazor.Constants;
+
+namespace ProjectHub.Blazor.Shared;
+
+public partial class LoginDisplay
+{
+    private void BeginLogOut()
+    {
+        Navigation.NavigateToLogout(ProjectHubRoute.AuthenticationLogout);
+    }
+
+    private void BeginLogIn()
+    {
+        Navigation.NavigateToLogin(ProjectHubRoute.AuthenticationLogin);
+    }
+}
